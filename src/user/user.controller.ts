ef2a7a0ac113aba6905fc21e7b4c9400401e000e
tnmrs <promomyers@gmail.com>
2022-09-04 12:00:25 +0300

    user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,46 +1,54 @@
 import {
-    Body,
-    Controller,
-    Get,
-    HttpCode,
-    Param,
-    Put,
-    UsePipes,
-    ValidationPipe
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Types } from 'mongoose';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { IdValidationPipe } from '../pipes/id.validation.pipe';
+import { CurrentUser } from './decorators/user.decorator';
 
-
-
-} from '@nestjs/common'
-import {Types} from 'mongoose'
-import {Auth} from '../auth/decotarors/auth.decorator'
-import {IdValidationPipe} from '../pipes/id.vadation.pipe'
-import {CurrentUser} from './decorators/user.decorator'
-
-import {UserDto} from './user.dto'
-import {UserService} from './user.service'
+import { UserDto } from './user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Get('profile')
-    @Auth()
-    async getProfile(@CurrentUser('_id') _id: Types.ObjectId) {
-        return this.userService.getUser(_id)
-    }
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrentUser('_id') _id: Types.ObjectId) {
+    return this.userService.getUser(_id);
+  }
+  //редактирование профиля
 
-    @Get('by-id/:id')
-    async getUser(@Param('id', IdValidationPipe) id: string) {
-        return this.userService.getUser(new Types.ObjectId(id))
-    }
-    
-    @UsePipes(new ValidationPipe())
-    @HttpCode(200)
-    @Put('profile')
-    @Auth()
-    async updateProfile(
-        @CurrentUser('_id') _id: Types.ObjectId
-        @Body() dto: UserDto
-    ) 
+  @Get('by-id/:id')
+  async getUser(@Param('id', IdValidationPipe) id: string) {
+    return this.userService.getUser(new Types.ObjectId(id));
+  }
 
+  //обновление профиля
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put('profile')
+  @Auth()
+  async updateProfile(@CurrentUser('_id') _id: Types.ObjectId, @Body() dto: UserDto) {
+    return this.userService.updateProfile(_id, dto);
+  }
+
+  //обновление пользователя для админа
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put(':id')
+  @Auth() //Admin
+  async updateUser(@Param('id', IdValidationPipe) id: Types.ObjectId, @Body() dto: UserDto) {
+    return this.userService.updateProfile(id, dto);
+  }
 }
